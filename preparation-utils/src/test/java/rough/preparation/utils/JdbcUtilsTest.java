c1d6a88f72fc182959c4f118950d6a2422d50820
周贇 <rough_zero@163.com>
2019-09-30 10:37:48 +0800

    增加测试 Orcle 生僻字

--- preparation-utils/src/test/java/rough/preparation/utils/JdbcUtilsTest.java
+++ preparation-utils/src/test/java/rough/preparation/utils/JdbcUtilsTest.java
@@ -1,6 +1,7 @@
 package rough.preparation.utils;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import junit.framework.TestCase;
@@ -21,6 +22,17 @@ public class JdbcUtilsTest extends TestCase {
         sql.append(" )");
         Statement st = conn.createStatement();
         st.execute(sql.toString());
+
+        conn = JdbcUtils.createConnection("oracle.jdbc.driver.OracleDriver",
+                "jdbc:oracle:thin:@(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.32.66.17)(PORT = 1521)) (ADDRESS = (PROTOCOL = TCP)(HOST = 10.32.66.18)(PORT = 1521)) ) (LOAD_BALANCE = yes) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = shdw) ) )",
+                "fin_look", "fin_look");
+        sql = new StringBuffer();
+        sql.append("select unistr('\\d845\\deb8') from dual");
+        st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql.toString());
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
     }
 
 }
